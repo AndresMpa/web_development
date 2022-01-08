95a2e8bafb575599899aee3dc5fa7eb10124168a
AndresMpa <andresprieto2703@gmail.com>
2022-01-07 23:51:36 -0500

    Adding methods to record and write to support different browsers

--- backend/audioToText/src/AudioToText.js
+++ backend/audioToText/src/AudioToText.js
@@ -2,24 +2,42 @@ class AudioToText {
   constructor(config) {
     this.browser = config.browser;
     this.recordMethod = config.recordMethod;
-    this.plugin = config.plugin || [];
+    this.recordPlugin = config.recordPlugin;
+    this.writerPlugin = config.writerPlugin;
 
-    this.records = [];
+    this.recordedFile = Object;
+    this.currentText = "";
   }
 
   record() {
-    console.log(`Recording on ${this.browser}`);
-    this.records.append(this.plugin[0]());
-  }
-
-  voiceTranscription() {
-    let transcription = "No voices...";
-
-    return transcription;
+    this.recordPlugin.prototype.heard(this);
   }
 
   get transcription() {
-    return this.voiceTranscription();
+    this.writerPlugin.prototype.write(this);
+    return this.currentText;
+  }
+
+  TTSRecord() {
+    console.log(`Recording on ${this.browser}, using ${this.recordMethod}`);
+    let file = "File";
+    this.recordedFile = file;
+  }
+
+  TTSWritter() {
+    console.log(`Writing from ${this.recordMethod} out put`);
+    this.currentText = "No yet";
+  }
+
+  WebRecord() {
+    console.log(`Recording on ${this.browser}, using ${this.recordMethod}`);
+    let file = "File"
+    this.recordedFile = file;
+  }
+
+  WebWritter() {
+    console.log(`Writing from ${this.recordMethod} out put`);
+    this.currentText = "No yet";
   }
 }
 
