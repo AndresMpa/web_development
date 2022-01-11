81d8bb3501eb3646afb6de0a075b1eab5aec4b9b
AndresMpa <andresprieto2703@gmail.com>
2022-01-11 13:23:27 -0500

    I integrate completly a functionaliyty to become speeches to texts

---
+++ backend/audioToText/src/util/WebSpeech.js
@@ -0,0 +1,94 @@
+const SpeechRecognitionEvent =
+  globalThis.SpeechRecognitionEvent || globalThis.webkitSpeechRecognitionEvent;
+
+class WebSpeech {
+  constructor(dictionary) {
+    this.dictionary = dictionary;
+    this.resutl = "";
+
+    this.recognitionList;
+    this.recognition;
+    this.grammar;
+
+    this.__initGrammar__();
+    this.__initRecognizer__();
+  }
+
+  // Conficts
+
+  __initGrammar__() {
+    const dictionary = this.dictionary;
+    this.grammar =
+      "#JSGF V1.0; grammar dictionary; public <dictionary> = " +
+      dictionary.join(" | ") +
+      " ;";
+  }
+
+  __initRecognizer__() {
+    const SpeechRecognition =
+      globalThis.SpeechRecognition || globalThis.webkitSpeechRecognition;
+    const SpeechGrammarList =
+      globalThis.SpeechGrammarList || globalThis.webkitSpeechGrammarList;
+
+    this.recognitionList = new SpeechGrammarList();
+    this.recognitionList.addFromString(this.grammar, 1);
+
+    this.recognition = new SpeechRecognition();
+    this.recognition.interimResults = true;
+    this.recognition.maxAlternatives = 1;
+    this.recognition.continuous = false;
+    this.recognition.lang = "es-CO";
+  }
+
+  // Getter
+
+  get getResult() {
+    return this.results ? this.results : "";
+  }
+
+  // Handlers
+
+  startHandler() {
+    this.recognition.start();
+  }
+
+  resultHandler() {
+    this.recognition.onresult = (event) => {
+      this.results = event.results[0][0].transcript + " ";
+    };
+  }
+
+  endHandler() {
+    this.recognition.onspeechend = () => {
+      this.recognition.stop();
+    };
+  }
+  errorhHandler() {
+    this.recognition.onnomatch = (event) => {
+      console.log(event);
+    };
+
+    this.recognition.onerror = (event) => {
+      console.log(event);
+    };
+  }
+}
+
+let sr = new WebSpeech(["hola", "esta", "es", "una", "nueva", "prueba"]);
+
+const recorder = document.querySelector("#recorder");
+const record = document.querySelector("#record");
+const test = document.querySelector("#test");
+
+recorder.onmousedown = () => {
+  sr.startHandler();
+  sr.resultHandler();
+};
+
+recorder.onmouseup = () => {
+  setTimeout(() => {
+    sr.endHandler();
+    sr.errorhHandler();
+    test.value += sr.getResult;
+  }, 800);
+};
